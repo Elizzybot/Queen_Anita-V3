e2ff865ae0ae609923947bc91b09d35df5eb0611
Elizzybot <elijahchinecheremonah@gmail.com>
2024-10-12 12:46:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ const fs = require('fs')
 const { color } = require('./lib/myfunc')
 
 //owner
-global.owner = '2347043759577'
-global.nomerowner = ["2347043759577"]
+global.owner = '2349070240348'
+global.nomerowner = ["2349070240348"]
 
 // Apikey 
 
-global.skizo = 'David Cyril'
+global.skizo = 'wise king'
 global.casterix = 'DeeCeeXxx'
 //watermark 
-global.packname = '*DAVID CYRIL*'
+global.packname = '*wise king*'
 global.author = 'QUEEN_ANITA-V3'
 
 // cpanel 
